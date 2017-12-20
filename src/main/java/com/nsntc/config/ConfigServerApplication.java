bdcc1bf247eab779e901737255fae040ac8a08ba
wkm <813272417@qq.com>
2017-12-20 18:25:18 +0800

    配置文件服务

--- src/main/java/com/nsntc/config/ConfigServerApplication.java
+++ src/main/java/com/nsntc/config/ConfigServerApplication.java
@@ -7,12 +7,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 /**
  * Class Name: ConfigServerApplication
  * Package: com.nsntc.config
- * Description: 
+ * Description: 开启配置服务
  * @author wkm
  * Create DateTime: 2017/12/16 上午8:46
  * Version: 1.0
  */
-/** 开启配置服务 */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
